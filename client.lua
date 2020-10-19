945108a314a1df29f4548d9b23f489866932007f
Paul <officialdrgaming@gmail.com>
2020-10-19 19:29:19 +0100

    Master

---
+++ client.lua
@@ -0,0 +1,316 @@
+CF = nil
+
+Citizen.CreateThread(function()
+	while CF == nil do 
+        Citizen.Wait(0)
+        TriggerEvent('cf:getSharedObject', function(obj) CF = obj end)
+    end
+    
+end)
+
+local Realitors = {"Paul R","Agent J","Mady M",'Easton D','W.White'}
+local locations = {
+
+
+    --[[ SINGLE OWNER EXAMPLE
+		{
+			name = 'Test',
+			price = '$999,999',
+			date = '09/13/2019',
+			owner = 'test',
+			x = -1508.78,
+			y = 1733.73,
+			z = 109.77,
+		   },
+
+		MULTIOWNER EXAMPLE
+		{
+			name = 'Test',
+			price = '$1,500,000',
+			date = '09/16/2019',
+			owner = nil,
+			owners = {'test1 - 13.33%', 'test2 - 16.66%', 'test3 - 16.66%', 'test4 - 13.33%', 'test5 - 13.33%', 'test6 - 6.66%', 'test7 - 6.66%', 'test8 - 13.33%'},
+			x = 2452.89,
+			y = 4951.3,
+			z = 45.14,
+		},
+	]]--
+        -- Use /propertycoords to grab the coordinates, they auto save into your clipboard after you run the command and then press t. Then you can just do lctrl + v to paste.
+
+	
+}
+
+
+
+
+
+Citizen.CreateThread(function()
+	TriggerServerEvent("GetProperties")
+end)
+
+function HelpText(text)
+    SetTextComponentFormat("STRING")
+    AddTextComponentString(text)
+    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
+
+RegisterNetEvent("AddBlips")
+AddEventHandler("AddBlips", function(source,ID)
+	print(ID)
+	print(table.unpack(source))
+	while true do
+		Citizen.Wait(0)
+        local _inside_marker = false
+		for k,v in pairs(source) do 
+			local property = source[k]
+			local data = source[k].data
+			local xt = json.decode(data)["x"]
+			local x = table.unpack(xt)
+			local zt = json.decode(data)["z"]
+			local z = table.unpack(zt)
+			local yt = json.decode(data)["y"]
+			local y = table.unpack(yt)
+			local ply_coords = GetEntityCoords(GetPlayerPed(-1), true)
+			if (GetDistanceBetweenCoords(ply_coords, x,y,z, true) < 15.0) then -- If true, render marker.
+				
+                DrawMarker(23, x,y,z-1, 0.0, 0.0, 0.0, 180.0, 0.0, 0.0, 1.50, 1.50, 1.50, 0, 100, 255, 150, false, true, 2, false, false, false, false)
+                if (GetDistanceBetweenCoords(ply_coords, x,y,z, true) < 1.25) then -- If true, show marker's action prompt.
+					_inside_marker = true
+					HelpText('Press ~o~~INPUT_PICKUP~~s~ to view ~r~' .. property.name .. '~s~ info.')
+					
+					if (IsControlJustReleased(0, 51)) then -- Interaction
+						menu(property,ID)
+					end
+                end
+			end
+		end
+        inside_marker = _inside_marker
+    end
+end)
+
+
+
+function HelpText(text)
+    SetTextComponentFormat("STRING")
+    AddTextComponentString(text)
+    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
+
+
+function menu(property,ID)
+	local place = property
+	local name = property.name
+	local _elements = {}
+    table.insert(_elements, {label = "<span style='color: #ffffff'><b>Name: " .. property.name .. "</b>",   value = 'name'})
+    table.insert(_elements, {label = "<span style='color: #ffffff'><b>Date: " .. property.date .. "</b>",   value = 'date'})
+	table.insert(_elements, {label = "<span style='color: #ffffff'><b>Sold For: " .. property.price .. "</b>",   value = 'price'})
+	if property.owner ~= nil then 
+		table.insert(_elements, {label = "<span style='color: #ffffff'><b>Owner: " .. property.owner .. "</b>",   value = 'owner'})
+	else
+		local owner = json.decode(place.owners)
+		local val = json.decode(place.identifiers)["value"]
+		print(#owner)
+		for i=1, #owner, 1 do
+			print(owner[i])
+			table.insert(_elements, {label = "<span style='color: #ffffff'><b>Owner: " .. owner[i] .. " - " .. val[i] .. "%</b>",   value = 'owner'})
+		end
+	end
+    local ident = json.decode(place.identifiers)["identiy"]
+	
+	
+	for k,v in ipairs(ident) do
+		print(ident[k])
+		if ID == ident[k]then
+			table.insert(_elements, {label = "<span style='color: color: #ffffff';><b>Owner Menu</b>",   value = 'ownerMenu'})
+		end
+
+	end
+
+    table.insert(_elements, {label = '<span style="color: red"><b>Exit</b></span>',       value = 'exit'})
+    
+
+    CF.UI.Menu.CloseAll()
+
+    CF.UI.Menu.Open('default1', GetCurrentResourceName(), 'Prop_Menu', {
+        title    = name,
+        align    = 'center',
+        elements = _elements,
+	},function(data, menu)
+        if data.current.value == 'exit' then
+			menu.close()
+		
+		elseif data.current.value == "ownerMenu" then
+			OwnerMenu(property,ID,name)
+		end
+        end)
+
+end
+
+
+
+function OwnerMenu(property,ID,name)
+	local place = property
+	local name = property.name
+	local _elements1 = {}
+	print(place.identifiers)
+	local owner = json.decode(place.owners)
+	local ident = json.decode(place.identifiers)["identiy"]
+	local val = json.decode(place.identifiers)["value"]
+	for k,v in ipairs(ident) do
+		if ident[k] == ID then
+			if val[k] >= 51 then
+				table.insert(_elements1, {label = '<span style="color: white"><b>Change Name</b></span>',       value = 'change'})
+			end
+		end
+	end
+	table.insert(_elements1, {label = '<span style="color: white"><b>Sell to someone</b></span>',       value = 'ST'})
+	
+	for k,v in ipairs(ident) do
+		if ident[k] == ID then
+			table.insert(_elements1, {label = '<span style="color: white"><b>You own '..val[k] ..'% </b></span>',       value = 'you'})
+		end
+	end
+	table.insert(_elements1, {label = '<span style="color: red"><b>Exit</b></span>',       value = 'exit'})
+
+	CF.UI.Menu.CloseAll()
+
+    CF.UI.Menu.Open('default1', GetCurrentResourceName(), 'Owner_menu', {
+	title    = "Owner Menu",
+        align    = 'center',
+        elements = _elements1,
+	},function(data, menu)
+        if data.current.value == 'exit' then
+			menu.close()
+		
+		elseif data.current.value == "change" then
+			ChangeName(name)
+		elseif data.current.value == "ST" then
+			SellTO(name,property,ID)
+		end
+		
+        end)
+end
+
+
+
+function ChangeName(name)
+	CF.UI.Menu.CloseAll()
+	CF.ShowNotification("To cancel press confirm while box is empty")
+	
+
+    CF.UI.Menu.Open('dialog', GetCurrentResourceName(), 'Change_Name', {
+		title    = "Name Change (Will change on the next restart)",
+        align    = 'center',
+        elements = _elements1,
+	},function(data, menu)
+		print(table.unpack(menu))
+		
+		if data.value ~= nil then
+			local NewName = data.value
+			TriggerServerEvent("UpdateName", NewName ,name)
+			menu.close()
+		else
+      		menu.close()
+		end
+
+        end)
+end
+
+
+function SellTO(name,property,ID)
+	CF.UI.Menu.CloseAll()
+	CF.ShowNotification("To cancel press confirm while box is empty")
+	local place = property
+	local ident = json.decode(place.identifiers)["identiy"]
+	local val = json.decode(place.identifiers)["value"]
+	for k,v in ipairs(ident) do
+		if ident[k] == ID then
+			amount = val[k]
+		end
+	end
+	
+    CF.UI.Menu.Open('dialog', GetCurrentResourceName(), 'Sell_To', {
+		title    = "You own "..amount .." %. How much are you selling?" ,
+        align    = 'center',
+        elements = _elements1,
+	},function(data, menu)
+		
+		print(table.unpack(menu))
+		if data.value ~= nil then
+			if tonumber(data.value) ~= nil then
+				local toChange = tonumber(data.value)
+				if toChange > amount then
+					CF.ShowNotification("~r~You do not own enough of this property")
+					menu.close()
+				else	
+					if toChange ~= amount then
+						if #ident >= 4 then 
+							CF.ShowNotification("~r~You can only have 4 people on a property")
+						else
+							Confirm(name,amount,id,property,toChange)
+						end
+					else 
+						Confirm(name,amount,id,property,toChange)
+					end
+				end
+			menu.close()
+			end
+		else
+      		menu.close()
+		end
+
+        end)
+end
+
+
+
+function Confirm(name,amount,id,property,toChange)
+	local _elements = {}
+	local am = toChange
+	table.insert(_elements, {label = "<span style='color: #ffffff'><b>Name: " .. property.name .. "</b>",   value = 'name'})
+	table.insert(_elements, {label = "<span style='color: #ffffff'><b>You are selling: " .. am .. "%</b>",   value = 'name'})
+	table.insert(_elements, {label = "<span style='color: #00ff08'><b>Confirm</b>",   value = 'Yes'})
+	table.insert(_elements, {label = "<span style='color: #ff0000'><b>Cancel</b>",   value = 'exit'})
+	CF.UI.Menu.CloseAll()
+    CF.UI.Menu.Open('default1', GetCurrentResourceName(), 'Confirm', {
+		title    = "Owner Menu",
+        align    = 'center',
+        elements = _elements,
+	},function(data, menu)
+        if data.current.value == 'exit' then
+			menu.close()
+		elseif data.current.value == "Yes" then
+			GetPer(name,amount,id,property,toChange)
+		end
+		
+    end)
+end
+
+
+
+function GetPer(name,amount,id,property,toChange)
+	CF.UI.Menu.CloseAll()
+	CF.ShowNotification("To cancel press confirm while box is empty")
+	local place = property
+	local ident = json.decode(place.identifiers)["identiy"]
+	local val = json.decode(place.identifiers)["value"]
+	for k,v in ipairs(ident) do
+		if ident[k] == ID then
+			amount = val[k]
+		end
+	end
+	
+    CF.UI.Menu.Open('dialog', GetCurrentResourceName(), 'GetPer', {
+		title    = "You own "..amount .." %. Who are you selling to?" ,
+        align    = 'center',
+        elements = _elements1,
+	},function(data, menu)
+		local person = data.value
+		if person ~= nil then
+			TriggerServerEvent("CheckPerson",name,amount,id,property,toChange,person)
+			menu.close()
+		else
+			CF.ShowNotification("~r~This input is not a number try again")
+		end
+    end)
+end
---
+++ server.lua
@@ -0,0 +1,245 @@
+CF = nil
+
+Citizen.CreateThread(function()
+	while CF == nil do 
+        Citizen.Wait(0)
+        TriggerEvent('cf:getSharedObject', function(obj) CF = obj end)
+    end
+    
+end)
+
+local locations = {
+		{
+			name = 'Little White Church',
+			price = '$450,000',
+			date = '08/18/2020',
+			owner = nil,
+            owners = {'Ray Y. - 50%', 'J. Cole - 50%'},
+			x = -323.8,
+			y = 2772.13,
+			z = 62.6,
+			ident = {identiy  = {"license:3644574414cbc484df25f5d756ae6b8256824e9e","license:1ad446d864d35da8eed6e3288ff1b0d56a2aa330"}, value = {50,50}},
+		},
+		{
+			name = 'GOH 24/7',
+			price = '$450,000',
+			date = '08/18/2020',
+			owner = nil,
+			owners = {'Ray Y. - 50%', 'Mike S. - 50%'},
+			x = -3037.04,
+			y = 588.07,
+			z = 7.82,
+			ident = {identiy  = {"license:3644574414cbc484df25f5d756ae6b8256824e9e","license:af779157a54104be9b3a8d292ca4fc13de49aedd"}, value = {50,50}},
+		},
+		{
+			name = 'Senora 24/7',
+			price = '$450,000',
+			date = '08/18/2020',
+			owner = nil,
+			owners = {'Ray Y. - 50%', 'Chris D. - 50%'},
+			ident = {identiy  = {"license:3644574414cbc484df25f5d756ae6b8256824e9e","license:bf7ce597dc5ae4fc921cd644f85ddc86a6e5fd5e"}, value = {50,50}},
+			x = 1723.5,
+			y = 6409.42,
+			z = 34.19,
+		},	
+		{
+			name = 'Sons of Anarchy MC -6',
+			price = '$825,000',
+			date = '08/18/2020',
+			owner = 'Sam W.',
+			x = -358.46,
+			y = 6324.24,
+            z = 29.81,
+            ident = {identiy  = {"license:260969b3730bcead7af92fa25d89283f92c08613"}, value = {100}},
+		},
+		{
+			name = 'Slatt And Lucy\'s Crib',
+			price = '$2,000,000',
+			date = '09/10/2020',
+			owner = nil,
+			owners = {'Slattius - 50%', 'Lucy H - 50%'},
+			x = -819.15,
+			y = 814.89,
+            z = 200.67,
+            ident = {identiy  = {"license:9e3e2db5fde0963f0f5b2df2e3936214bc9d7323","license:90d38cbc7b1c0b53fcf52185d8002895de737879"}, value = {50,50}},
+		},
+		{
+			name = 'Rt 68 24/7',
+			price = '$425,000',
+			date = '08/21/2020',
+			owner = nil,
+			owners = {'Ray - 80%', 'Chris D - 10%', 'Mike S - 10%'},
+			x = 543.24,
+			y = 2677.9,
+            z = 42.23,
+            ident = {identiy  = {"license:3644574414cbc484df25f5d756ae6b8256824e9e","license:bf7ce597dc5ae4fc921cd644f85ddc86a6e5fd5e","license:af779157a54104be9b3a8d292ca4fc13de49aedd" }, value = {80, 10 ,10 }},
+		},
+		{
+		
+			name = 'Sons of Anarchy MC -7',
+			price = '$700,000',
+			date = '08/21/2020',
+			owner = nil,
+			owners = {'Sam W - 50%', 'Jack N - 50%'},
+			x = 795.39,
+			y = -2128.03,
+            z = 29.46,
+            ident = {identiy  = {"license:260969b3730bcead7af92fa25d89283f92c08613","license:2733cbb6c53e00e614de289cecd015cb3872dcf2"}, value = {50,50}},
+		},
+		{
+			name = 'BNR Traphouse -2',
+			price = '$525,000',
+			date = '08/23/20',
+			owner = nil,
+			owners = {'A. Ramirez - 50%', 'Braden S - 50%'},
+			x = -0.07,
+			y = 6547.17,
+            z = 31.55,
+            ident = {identiy  = {"license:daa3abdc0c0b894e390c42da76a74362f63a2864","license:4594018e40d58986da2595fdab04744f77b225e9"}, value = {50, 50}},
+		},
+		{
+			name = 'Jack\'s Crackhouse',
+			price = '$1,500,000',
+			date = '08/24/20',
+			owner = 'Jack D',  
+			x = 3325.48,
+			y = 5168.13,
+            z = 18.40,
+            ident = {identiy  = {"license:4f212000921477c7f5f850152df01d501bc76853"}, value = {100}},
+		},
+		{
+			name = 'The Big D Dealership',
+			price = '$1,100,000',
+			date = '08/24/20',
+			owner = nil,
+			owners = {'Darius - 75%', 'Jesus Derulo - 15%', 'G. Davis - 10%'},
+			x = -194.62,
+			y = -1395.85,
+            z = 31.1,
+            ident = {identiy  = {"license:2aed4606429bf113e7b61420179bde48642d3f6f","license:33d794ceb313b80c3239cd2e63a8ea8eb0b45723","license:b2fb1f3e4a536a6f7422099471634df3cb3eb7ef" }, value = {75, 10 ,15 }},
+		},
+		{
+			name = 'StreetRunners',
+			price = '$2,000,000',
+			date = '08/27/20',
+			owner = nil,
+			owners = {'A. Ramirez - 75%', 'Braden S. - 25%'},
+			x = -998.76,
+			y = 794.91,
+            z = 171.79,
+            ident = {identiy  = {"license:daa3abdc0c0b894e390c42da76a74362f63a2864","license:4594018e40d58986da2595fdab04744f77b225e9"}, value = {75, 25}},
+		},
+		{
+			name = 'Sons of Anarchy MC -8',
+			price = '$1,750,000',
+			date = '08/20/2020',
+			owner = 'Sam W.',
+			x = -20.73,
+			y = -1390.74,
+            z = 29.39,
+            ident = {identiy  = {"license:260969b3730bcead7af92fa25d89283f92c08613"}, value = {100}},
+            
+		},
+		{
+			name = 'Vagos Trap',
+			price = '$300,000',
+			date = '09/18/2020',
+			owner = nil,
+			owners = {'Trey J. - 50%', 'J. Santos - 50%'},
+			x = -373.15,
+			y = 6181.2,
+            z = 31.5,
+            ident = {identiy  = {"license:a1f30ed6a3e47452e2688e19838c11153d5756b9","3AA"}, value = {50, 50}},
+		},
+
+}
+--[[Citizen.CreateThread(function()
+	for k,v in ipairs(locations) do
+		print(k)
+		local name = locations[k].name
+		local price = locations[k].price
+		local date = locations[k].date
+		local owner = locations[k].owner
+		local owners = json.encode(locations[k].owners)
+		local ident = json.encode(locations[k].ident)
+		local coords = json.encode({x = {locations[k].x},y = {locations[k].y},z = {locations[k].z}})
+		MySQL.Async.execute('INSERT INTO properties (`name`,`price`,`owner`,`owners`,`date`,`identifiers`,`data`) VALUES (@name,@price,@owner,@owners,@date,@ident,@coords);',
+		{
+			["@name"] = name,
+			["@price"] = price,
+			["@owner"] = owner,
+			["@owners"] = owners,
+			["@date"] = date,
+			["@ident"] = ident,
+			["@coords"] = coords
+		})
+		print("Added Added")
+		Citizen.Wait(100)
+	end
+end)   ]]    
+
+RegisterServerEvent("GetProperties")
+AddEventHandler("GetProperties", function()
+	_source = source
+	ID = GetPlayerIdentifier(_source, 1)
+	print(_source .. " Source")
+	MySQL.Async.fetchAll('SELECT * FROM properties', {}, function(players)
+		print(players)
+		print(ID)
+		TriggerClientEvent("AddBlips", _source,players,ID)
+	end)
+	print("players")
+	print(tab)
+end)
+
+
+--,`price`,`owner`,`owners`,`date`,`identifiers`,`Coordinates`
+--,@price,@owner,@owners,@date,@ident,@coords)
+
+RegisterServerEvent("UpdateName")
+AddEventHandler("UpdateName", function(data,name)
+	print(data)
+	print(name)
+	MySQL.Async.fetchAll('UPDATE properties SET name=@name WHERE name = @Cname', {
+		["@name"] =data,
+		["@Cname"] = name,
+	})
+end)
+
+
+RegisterServerEvent("SellTo")
+AddEventHandler("SellTo", function(name,amount,id,property,toChange,person,check)
+	print(name .. "Propery Name")
+	print(amount .. "Total")
+	print(id .. "ID")
+	print(property)
+	print(toChange .. "To Change")
+	print(person .. "Going to")
+	print(check .. "Persons name")
+	local ident = json.decode(property.identifiers)["identiy"]
+	local val = json.decode(property.identifiers)[""]
+	for k,v in ipairs(ident) do
+		local CID = ident[k]
+		local PID = GetPlayerIdentifier(id,1)
+		if CID == PID and #ident == 4 then
+			print()
+		else
+			print("OFF")
+		end
+	end
+	
+end)
+
+RegisterServerEvent("CheckPerson")
+AddEventHandler("CheckPerson", function(name,amount,id,property,toChange,person)
+	local id = source
+	local check = GetPlayerName(person)
+	if check ~= nil then
+		giver = GetPlayerName(source)
+		CF.ShowNotification(source,"~g~You have transfered " .. toChange .. "% of the property to " .. check .. "~g~ You have ".. amount-toChange .. "% remaining")
+		CF.ShowNotification(person,"~g~You have been given " .. toChange.. "% of ".. name .. " by ".. giver)
+		TriggerEvent("SellTo",name,amount,id,property,toChange,person,check)
+	else
+		CF.ShowNotification(source,"~r~This person is not online")
+	end
+end)
